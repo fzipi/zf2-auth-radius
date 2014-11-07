18ec04b8047be11f9bb8a76faafe09f934bdba67
Felipe Zipitría <fzipi@fing.edu.uy>
2014-11-07 12:04:02 -0200

    bad copy & paste

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,24 +1,24 @@
 <?php
 /**
-* Zend Framework (http://framework.zend.com/)
-*
-* @link http://github.com/zendframework/zf2 for the canonical source repository
-* @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
-* @license http://framework.zend.com/license/new-bsd New BSD License
-* @package Zend
-*/
+ * Fing Radius Auth ZF2 Adapter
+ *
+ * @link http://github.com/
+ * @copyright Copyright (c) 2014 Felipe Zipitria
+ * @license MIT License
+ * @package Fing
+ */
 
 /*
-* Load the user-defined test configuration file, if it exists; otherwise, load
-* the default configuration.
-*/
-if (is_readable($zfCoreTests . DIRECTORY_SEPARATOR . 'TestConfiguration.php')) {
-require_once $zfCoreTests . DIRECTORY_SEPARATOR . 'TestConfiguration.php';
+ * Load the user-defined test configuration file, if it exists; otherwise, load
+ * the default configuration.
+ */
+if (is_readable('TestConfiguration.php')) {
+    require_once 'TestConfiguration.php';
 } else {
-require_once $zfCoreTests . DIRECTORY_SEPARATOR . 'TestConfiguration.php.dist';
+    require_once 'TestConfiguration.php.dist';
 }
 
 /**
-* Setup autoloading
-*/
+ * Setup autoloading
+ */
 include __DIR__ . '/../vendor/autoload.php';
